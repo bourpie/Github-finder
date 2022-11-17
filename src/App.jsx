aad920e326d1b4e29b4d68204b5d665e482dd2f4
pbourgeois <pbcomweb@gmail.com>
2022-11-17 08:13:09 -0500

    search and clear users

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,33 @@
-import UserResults from './components/users/UserResults'
+import React from 'react'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Navbar from './components/layout/Navbar'
+import Footer from './components/layout/Footer';
+import Home from './pages/Home';
+import About from './pages/About';
+import NotFound from './pages/NotFound';
+import { GithubProvider } from './context/github/GithubContext';
 
 function App() {
  
   return (
-    <>
-      <UserResults />   
-    </>
+    <GithubProvider>
+      <Router>
+        <div className='flex flex-col h-screen'>
+          <Navbar />
+          <main className='pb-16 mb-auto'>
+            <div className="container">
+              <Routes>
+                <Route path='/' element={ <Home />} />
+                <Route path='/about' element={ <About />} />
+                <Route path='/notfound' element={ <NotFound />} />
+                <Route path='/*' element={ <NotFound />} />
+              </Routes>
+            </div>
+          </main>
+          <Footer />
+        </div>
+      </Router>
+    </GithubProvider>
   )
 }
 
